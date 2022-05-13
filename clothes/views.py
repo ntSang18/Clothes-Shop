5f927f48dfbcd488afdf8e07aac12a72acdad1e2
ThanhSang <thanhsang6325@gmail.com>
2022-05-14 00:55:47 +0700

    register

--- clothes/views.py
+++ clothes/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from hashlib import new
+from unicodedata import name
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Account, User
 
-# Create your views here.
+
+def index(request):
+    return render(request, template_name='clothes/index.html')
+
+
+def navRegister(request):
+    return render(request, 'clothes/register.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_name = request.POST['user_name']
+        phone = request.POST['phone']
+        password = request.POST['password']
+        new_account = Account()
+        new_account.user_name = user_name
+        new_account.phone = phone
+        new_account.password = password
+        new_account.save()
+        return render(request, 'clothes/index.html')
+    else:
+        return render(request, 'clothes/register.html')
