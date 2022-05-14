98d797177b6961484f01a72c13f93843bb796635
ThanhSang <thanhsang6325@gmail.com>
2022-05-14 22:37:32 +0700

    change models

--- clothes/admin.py
+++ clothes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Account
+from .models import User, Product, Product_type
 
-admin.site.register(Account)
 admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Product_type)
--- clothes/models.py
+++ clothes/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 
 
-class Account(models.Model):
-    user_name = models.CharField(max_length=100, unique=True)
-    phone = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=200)
+class User(models.Model):
+    user_name = models.CharField(max_length=200, unique=True)
+    passworld = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    full_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.user_name
 
 
-class User(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=100)
-    gender = models.BooleanField
-    birthday = models.DateField
-    email = models.EmailField
+class Product_type(models.Model):
+    product_type = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.product_type
+
+
+class Product(models.Model):
+    product_name = models.TextField(max_length=200)
+    image = models.ImageField(upload_to="images/")
+    price = models.IntegerField()
+    froduct_for_male = models.BooleanField()
+    product_type = models.ForeignKey(Product_type, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.full_name
+        return self.product_name
